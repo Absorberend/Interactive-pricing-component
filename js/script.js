6debc3623184f41f6e3e4a8fabbf5eb536a24123
Absorberend <102243063+Absorberend@users.noreply.github.com>
2022-07-01 19:22:56 +0200

    edited slider

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ const discount = document.getElementById("discount");
 const Query = window.matchMedia('(max-width: 649px)');
 const views = document.getElementById('slider__views');
 
-let outputValue = 0;
-
+let outputValue = 16;
+let backupValue = 12;
+let toggle = false;
 
 //Add proper discount text
 function screenTest(e) {
@@ -19,63 +20,109 @@ function screenTest(e) {
   }
 }
 
-//Pageviews editor based on slider value
-const viewCheck = function () {
-    if (slider.value === '0') {
-        views.innerHTML = '0k';
-    } else if (slider.value === '4') {
-        views.innerHTML = '5k';
-    } else if (slider.value === '8') {
-        views.innerHTML = '10k';
-    } else if (slider.value === '12') {
-        views.innerHTML = '50k';
-    } else if (slider.value === '16') {
-        views.innerHTML = '100k';
-    } else if (slider.value === '20') {
-        views.innerHTML = '250k';
-    } else if (slider.value === '24') {
-        views.innerHTML = '500k';
-    } else if (slider.value === '28') {
-        views.innerHTML = '750k';
-    } else if (slider.value === '32') {
-        views.innerHTML = '1M';
-    }
+//Onload screentest
+window.onload = function () {
+    screenTest(Query);
+    output.innerHTML = '$' + outputValue + '.00';
 }
 
 //screentest after resize
 Query.addEventListener("change", screenTest);
 
 
-//Calculate the discounted  price
-const checkboxCheck = function () {
-    if (checkbox.checked === false) {
-        return output.innerHTML = `$${slider.value}.00`;
+//Instantly view price change on screen when checkbox is checked
+checkbox.addEventListener("input", function() {
+    if (toggle === false) {
+    output.innerHTML = '$' + backupValue + '.00';
+    toggle = true;
     } else {
-        const sliderOutput = slider.value * 0.75;
-        return output.innerHTML = `$${sliderOutput}.00`;   
-    } 
-}
-
-//When checkbox is checked show the discounted price
-checkbox.oninput = function () {
-    checkboxCheck();
+    output.innerHTML = '$' + outputValue + '.00';   
+    toggle = false;
     }
-
-//When slider is moved show the new price based on slider value
-slider.oninput = function () {
-    checkboxCheck();   
-}
-
-//Change the slider track based on the slider value
-slider.addEventListener("input", function() {
-    //Slider max value is 32. 100 / 32 = 3.125. So a slider value of 16 (50% of max slider value) * 3,125 is 50% gradiant %.
-    viewCheck();
-    const gradientTrack = slider.value * 3.125;
-    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${gradientTrack}%, hsl(224, 65%, 95%)${gradientTrack}%)`;
 });
 
-//Onload screentest
-window.onload = function () {
-    screenTest(Query);
-    output.innerHTML = `$${slider.value}.00`;
-}
+slider.addEventListener("input", function() {
+    let track = slider.value;
+
+    //Change slider track color based on slider value.
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${track}%, hsl(224, 65%, 95%)${track}%)`;
+
+    //View prices based on slider value and if checkbox is checked.
+    if (!checkbox.checked) {
+        if (track === '0') {
+            outputValue = 8;
+            views.innerHTML = '10k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 6;
+            toggle = false;
+    
+        } else if (track === '25') {
+            outputValue = 12;
+            views.innerHTML = '50k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 9;
+            toggle = false;
+    
+        } else if (track === '50') {
+            outputValue = 16;
+            views.innerHTML = '100k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 12;
+            toggle = false;
+    
+        } else if (track === '75') {
+            outputValue = 24;
+            views.innerHTML = '500k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 18;
+            toggle = false;
+    
+        } else if (track === '100') {
+            outputValue = 36;
+            views.innerHTML = '1M';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 27;
+            toggle = false;
+        }
+    } else {
+        if (track === '0') {
+            outputValue = 6;
+            views.innerHTML = '10k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 8;
+            toggle = false;
+            
+        } else if (track === '25') {
+            outputValue = 9;
+            views.innerHTML = '50k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 12;
+            toggle = false;
+            
+        } else if (track === '50') {
+            outputValue = 12;
+            views.innerHTML = '100k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 16;
+            toggle = false;
+
+        } else if (track === '75') {
+            outputValue = 18;
+            views.innerHTML = '500k';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 24;
+            toggle = false;
+
+        } else if (track === '100') {
+            outputValue = 27;
+            views.innerHTML = '1M';
+            output.innerHTML = '$' + outputValue + '.00';
+            backupValue = 36;
+            toggle = false;
+        }
+    }
+});
+
+
+
+   
